bea381286dfa223ed4b28186df56a8f3146fc133
Nelson Murungi <nelsonmwirumubi@gmail.com>
2022-08-25 17:49:11 +0300

    Initial commit

---
+++ pages/_app.js
@@ -0,0 +1,11 @@
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }) {
+    return (
+        <div className="bg-gray-50 min-h-screen">
+            <Component {...pageProps} />
+        </div>
+    );
+}
+
+export default MyApp;
---
+++ pages/index.js
@@ -0,0 +1,3 @@
+export default function Home() {
+    return <div className="text-gray-800">Job Lisitings</div>;
+}
